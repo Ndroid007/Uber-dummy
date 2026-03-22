01a3b0551268ecb042ca34d95ae6320e37d9c743
anuragNC-del <anurag.mishra@nathcorp.com>
2026-03-22 09:09:10 +0530

    added captain register route

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
-const cookieParser = require('cookie-parser');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
@@ -20,7 +21,7 @@ app.get('/', function (req, res) {
 });
 
 app.use('/users', userRoutes);
-
+app.use('/captains', captainRoutes);
 
 
 module.exports = app
